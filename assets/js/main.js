2c50e37ae2fedbd1c6a72b9ba012b61f354b3816
vishnukkrishna <vishnukkrishna1999@gmail.com>
2024-08-10 19:05:17 +0530

    Style header and navigation with responsive design, including mobile menu functionality and hover effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// Menu Show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+// Menu Hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
